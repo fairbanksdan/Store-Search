3100fead472fcd65e145559eb82810415d24325a
Dan Fairbanks <fairbanksdan@gmail.com>
2014-11-13 18:45:59 -0800

    Added JSON API calls

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -11,4 +11,11 @@ import Foundation
 class SearchResult {
     var name = ""
     var artistName = ""
+    var artworkURL60 = ""
+    var artworkURL100 = ""
+    var storeURL = ""
+    var kind = ""
+    var currency = ""
+    var price = 0.0
+    var genre = ""
 }
